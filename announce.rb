de2ca87c3dbd34c754ca79b048d014e31455c77c
maeda-seina <ayrton19601027@gmail.com>
2022-03-08 20:29:06 +0900

    Discordサーバー内の全てチャンネル情報の取得

--- announce.rb
+++ announce.rb
@@ -4,4 +4,7 @@ require 'dotenv/load'
 bot = Discordrb::Bot.new(token: ENV['DISCORD_BOT_TOKEN'],
                          client_id: ENV['DISCORD_CLIENT_ID'])
 
+channels = Discordrb::API::Server.channels(ENV['DISCORD_BOT_TOKEN'],
+                                           ENV['DISCORD_SERVER_ID'])
+p JSON.parse(channels)
 bot.run
