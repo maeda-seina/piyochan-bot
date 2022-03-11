e6cb1a0b0e1c8d2921d90e81833701a6d26d4269
maeda-seina <ayrton19601027@gmail.com>
2022-03-11 23:39:20 +0900

    rubocop OK

--- lib/discord_api.rb
+++ lib/discord_api.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'discordrb'
 require 'dotenv/load'
 
@@ -6,9 +8,9 @@ module DiscordApi
                            client_id: ENV['DISCORD_CLIENT_ID'])
 
   ALL_CHANNELS = Discordrb::API::Server.channels(ENV['DISCORD_BOT_TOKEN'],
-                                             ENV['DISCORD_SERVER_ID'])
+                                                 ENV['DISCORD_SERVER_ID'])
 
-  def self.create_message(message,tts = false, embed_message)
+  def self.create_message(message, embed_message, tts: false)
     Discordrb::API::Channel.create_message(ENV['DISCORD_BOT_TOKEN'],
                                            ENV['DISCORD_RANDOM_CHANNEL_ID'],
                                            message,
