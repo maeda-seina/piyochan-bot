fc55faee8b524ebc3bb4899d1b1c70ff3d2f6ea6
maeda-seina <ayrton19601027@gmail.com>
2022-03-16 20:06:21 +0900

    Discordrb::Bot.newはする必要がないから削除し、関連する箇所も削除

--- lib/discord_api.rb
+++ lib/discord_api.rb
@@ -4,9 +4,6 @@ require 'discordrb'
 require 'dotenv/load'
 
 module DiscordApi
-  BOT = Discordrb::Bot.new(token: ENV['DISCORD_BOT_TOKEN'],
-                           client_id: ENV['DISCORD_CLIENT_ID'])
-
   ALL_CHANNELS = Discordrb::API::Server.channels(ENV['DISCORD_BOT_TOKEN'],
                                                  ENV['DISCORD_SERVER_ID'])
 
