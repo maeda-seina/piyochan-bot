0e91d5204e32fd0f1341f98eee86374d0fba09fd
maeda-seina <ayrton19601027@gmail.com>
2022-03-24 15:10:09 +0900

    rubocop OK

--- test/bot_message_test.rb
+++ test/bot_message_test.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative '../lib/discord_api'
 require_relative '../lib/channel_info'
 require_relative '../lib/bot_message_formatter'
 require_relative '../lib/bot_message'
 require 'webmock/minitest'
-include WebMock::API
 
 class BotMessageTest < Minitest::Test
+  include WebMock::API
+
   def setup
     WebMock.disable_net_connect!
   end
@@ -18,7 +21,7 @@ class BotMessageTest < Minitest::Test
   def test_post_message
     message_url = "#{Discordrb::API.api_base}/channels/#{ENV['DISCORD_RANDOM_CHANNEL_ID']}/messages"
     first_message = stub_request(:post, message_url)
-                      .with(body: hash_including({ content: 'こんにちは！今日オススメのチャンネルを紹介をするよ〜' }))
+                    .with(body: hash_including({ content: 'こんにちは！今日オススメのチャンネルを紹介をするよ〜' }))
     stub_message = stub_request(:post, message_url).with(body: hash_including(embed_hash))
     BotMessage.create
     assert_requested(first_message)
@@ -26,13 +29,14 @@ class BotMessageTest < Minitest::Test
   end
 
   private
+
   def embed_hash
     {
       embed: {
         title: BotMessageFormatter::EMBED_TITLE,
         description: BotMessageFormatter.new.send(:format_embed_description),
         color: 3_066_993
-      },
+      }
     }
   end
 end
