682fb459f6c2e84bf40ede2972dedd2f496dfd84
maeda-seina <ayrton19601027@gmail.com>
2022-03-24 17:00:35 +0900

    テストが通るよう設計を変更(bot_message_formatter.rbのcreate_embed_messageが引数を取れるよう修正し、テストファイルでテストするdescriptionの中身を固定できるようにした。)

--- test/bot_message_test.rb
+++ test/bot_message_test.rb
@@ -23,7 +23,7 @@ class BotMessageTest < Minitest::Test
     first_message = stub_request(:post, message_url)
                     .with(body: hash_including({ content: 'こんにちは！今日オススメのチャンネルを紹介をするよ〜' }))
     stub_message = stub_request(:post, message_url).with(body: hash_including(embed_hash))
-    BotMessage.create
+    BotMessage.create(embed_hash_description)
     assert_requested(first_message)
     assert_requested(stub_message)
   end
@@ -34,9 +34,13 @@ class BotMessageTest < Minitest::Test
     {
       embed: {
         title: BotMessageFormatter::EMBED_TITLE,
-        description: BotMessageFormatter.new.send(:format_embed_description),
+        description: embed_hash_description,
         color: 3_066_993
       }
     }
+  end
+
+  def embed_hash_description
+    "チャンネル名： [#ruby](https://discord.com/channels/933233655172726845/943713981581910036)\n" + "説明： rubyについていろいろお話ししましょう〜\n" + "https://www.ruby-lang.org/ja/"
   end
 end
