3b0cf2cd3102e32205441417b5b96bb4c577480b
maeda-seina <ayrton19601027@gmail.com>
2022-04-09 12:57:26 +0900

    `BotMessage.create(formatter)`と引数で受け取った情報を使って`DiscordApi.create_message(...)`が処理されるよう修正した

--- lib/bot_message.rb
+++ lib/bot_message.rb
@@ -4,8 +4,9 @@ require_relative 'bot_message_formatter'
 
 class BotMessage
   class << self
-    def create(description = nil)
-      DiscordApi.create_message(BotMessage.message, BotMessageFormatter.new.create_embed_message(embed_description: description))
+    def create(formatter)
+      DiscordApi.create_message(BotMessage::message,
+                                formatter)
     end
 
     def message
